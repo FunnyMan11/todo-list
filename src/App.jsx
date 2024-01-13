debde0774600b9ce151f996e814561af77b55e01
macbook air <macbookair@macbooks-MacBook-Air-2.local>
2024-01-13 13:37:31 +0400

    styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,40 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState('');
 
-  const [todoList, setTodoList] = useState([])
-  const [newTask, setNewTask] = useState("");
-
-  const handleChange = (event) =>{
+  const handleChange = (event) => {
     setNewTask(event.target.value);
-  }
+  };
 
-  const addTask = () =>{
-    setTodoList([...todoList, newTask])
-  }
+  const addTask = () => {
+    setTodoList([...todoList, newTask]);
+    setNewTask('');
+  };
 
-  const deleteTask =(taskName)=>{
-    const newTodoList = todoList.filter((task)=>{
-      if(task===taskName){
-        return false;
-      } else {
-        return true;
-      }
-    })
-
-    {/* return task !=== taskName */}
-
-    setTodoList(newTodoList)
-  }
+  const deleteTask = (taskName) => {
+    const newTodoList = todoList.filter((task) => task !== taskName);
+    setTodoList(newTodoList);
+  };
 
   return (
     <div className="App">
       <div className="addTask">
-        <input type="text" onChange={handleChange} />
-        <button onClick={addTask} >Add Task</button>
+        <input type="text" value={newTask} onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
       </div>
       <div className="list">
-        {todoList.map((task)=>{
-          return(
-            <div>
-              <h1>{task}</h1>
-              <button onClick={()=> deleteTask(task)} >X</button>
-            </div>
-          )
-        })}
+        {todoList.map((task) => (
+          <div key={task}>
+            <h1>{task}</h1>
+            <button onClick={() => deleteTask(task)}>X</button>
+          </div>
+        ))}
       </div>
     </div>
   );
 }
 
 export default App;
-
-
